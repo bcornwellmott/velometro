a30e9529e216f69fe5d9d77b488acc829e903c06
Ben Cornwell-Mott <bcornwellmott@velometro.com>
2016-08-01 08:26:19 -0700

    Added affect_bom functions

--- velometro/velometro/inventor_integration.py
+++ velometro/velometro/inventor_integration.py
@@ -31,3 +31,22 @@ def get_item_tags():
 				item_list.append(q)
 				
 	return ",".join(item_list)
+
+@frappe.whitelist()
+def get_affected_boms(item_code):
+	bom_list = []
+	frappe.msgprint("Running")
+	for d in frappe.db.sql("""select bom.name from `tabBOM` bom, `tabBOM Item` item where bom.name = item.parent and bom.is_active = 1 and item.item_code = %(item_code)s""",{"item_code":item_code}, as_dict=1):
+		frappe.msgprint("Got in the loop")
+		bom_list.append(d.name)
+	return bom_list
+	
+@frappe.whitelist()
+def get_affected_parent_boms(item_code):
+	bom_list = []
+	for d in frappe.db.sql("""select distinct bom.name, bom.item from `tabBOM` bom, `tabBOM Item` item where bom.name = item.parent and bom.is_active = 1 and item.item_code = %(item_code)s""",{"item_code":item_code}, as_dict=1):
+		bom_list.append(d.name)
+		for level_up in get_affected_parent_boms(d.item):
+			if level_up not in bom_list:
+				bom_list.append(level_up)
+	return bom_list
