2117c1dac3a0e5d9445fa1e075b5eb426ea2fd8d
Ben Cornwell-Mott <bcornwellmott@velometro.com>
2016-09-21 12:04:28 -0700

    Fixed adding docs to po and rfqs

--- velometro/velometro/purchase_order.py
+++ velometro/velometro/purchase_order.py
@@ -1,4 +1,3 @@
-
 from __future__ import unicode_literals
 
 import frappe
@@ -9,18 +8,30 @@ import json
 
 
 @frappe.whitelist()
-def attach_all_docs(document, method=None):
+def attach_all_docs(document):
 	"""This function attaches drawings to the purchase order based on the items being ordered"""
 	document = json.loads(document)
+	document2 = frappe._dict(document)
 	
 	current_attachments = []
-	for file_url in frappe.db.sql("""select file_url from `tabFile` where attached_to_doctype = %(doctype)s and attached_to_name = %(docname)s""", {'doctype': document["doctype"], 'docname': document["name"]}, as_dict=True ):
+	
+	for file_url in frappe.db.sql("""select file_url from `tabFile` where attached_to_doctype = %(doctype)s and attached_to_name = %(docname)s""", {'doctype': document2.doctype, 'docname': document2.name}, as_dict=True ):
 		current_attachments.append(file_url.file_url)
+	
+	# add the directly linked drawings
+	items = []
+	for item in document["items"]:
+		#frappe.msgprint(str(item))
+		items.append(item["item_code"])
+		
+		#add the child documents (from boms)
+		items = add_bom_items(items, item["item_code"])
+	
+	
 	count = 0
-	for item_doc in document["items"]:
+	for item_doc in items:
 		#frappe.msgprint(item_doc)
-		# Check to see if the quantity is = 1
-		item = frappe.get_doc("Item",item_doc["item_code"])
+		item = frappe.get_doc("Item",item_doc)
 		
 		attachments = []
 		# Get the path for the attachments
@@ -37,9 +48,17 @@ def attach_all_docs(document, method=None):
 			# Check to see if this file is attached to the one we are looking for
 			if not attach in current_attachments:
 				count = count + 1
-				save_url(attach, document["doctype"], document["name"], "Home/Attachments")
+				save_url(attach, document2.doctype, document2.name, "Home/Attachments")
 	frappe.msgprint("Attached {0} files".format(count))
 		
-				 
+def add_bom_items(items, item_code):
+	bom_num = frappe.get_value("Item",item_code, "default_bom")
+	if bom_num:
+		bom = frappe.get_doc("BOM",bom_num)
+		for bom_item in bom.items:
+			items.append(bom_item.item_code)
+			items = add_bom_items(items, bom_item.item_code)
+	return items
+			 
 
 
