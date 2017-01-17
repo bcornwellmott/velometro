303b03a75aef2eaf698e34fb09119b3bb35cc943
Ben Cornwell-Mott <bcornwellmott@velometro.com>
2017-01-16 21:27:33 -0800

    Created Work Instruction docs

---
+++ velometro/velometro/doctype/work_instructions/work_instructions.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Velometro Mobility Inc and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Work Instructions', {
+	refresh: function(frm) {
+
+	}
+});
