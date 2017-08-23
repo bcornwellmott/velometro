b31cda9f8a41eecffc884e5b18a627b044baa632
Ben Cornwell-Mott <bcornwellmott@velometro.com>
2017-08-23 08:55:52 -0700

    Fix reload_doc

--- velometro/patches/description_to_notes.py
+++ velometro/patches/description_to_notes.py
@@ -2,7 +2,7 @@ import frappe
 from frappe.utils import cint
 
 def execute():
-	frappe.reload_doc("Item")
+	frappe.reload_doc('stock', 'doctype', 'Item')
 	for item in frappe.get_all('Item', fields=["*"],	filters=[]):
 		item.notes = item.description
 		item.save()
