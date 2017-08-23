3c622417ca83cd969a901e0554a77d4fbdcc58c0
Ben Cornwell-Mott <bcornwellmott@velometro.com>
2017-08-23 10:31:53 -0700

    I know nothing

--- velometro/patches/description_to_notes.py
+++ velometro/patches/description_to_notes.py
@@ -2,7 +2,7 @@ import frappe
 from frappe.utils import cint
 
 def execute():
-	frappe.reload_doc('stock', 'doctype', 'Item')
+	frappe.reload_doc('stock', 'doctype', 'item')
 	for d in frappe.get_all('Item', fields=["name"], filters=[]):
 		item = frappe.get_doc("Item", d.name)
 		item.notes = item.description
