50b53072c436cac1846e0c476005f9fdd036c887
Ben Cornwell-Mott <bcornwellmott@velometro.com>
2017-08-23 08:58:28 -0700

    Fixed item lookup

--- velometro/patches/description_to_notes.py
+++ velometro/patches/description_to_notes.py
@@ -3,6 +3,7 @@ from frappe.utils import cint
 
 def execute():
 	frappe.reload_doc('stock', 'doctype', 'Item')
-	for item in frappe.get_all('Item', fields=["*"],	filters=[]):
+	for d in frappe.get_all('Item', fields=["name"], filters=[]):
+		item = frappe.get_doc("Item", d.name)
 		item.notes = item.description
 		item.save()
