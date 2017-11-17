8c9cffe7eb3d0a5a76c0652d59041fbb5f9b2c4a
Ben Cornwell-Mott <bcornwellmott@velometro.com>
2017-11-16 18:35:00 -0800

    Removed weekly review

--- velometro/velometro/web_form/weekly_performance_evaluation/weekly_performance_evaluation.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-import frappe
-
-def get_context(context):
-	user = frappe.get_value("User", {"email": frappe.session.user}, "name")
-	frappe.form_dict.employee = user
